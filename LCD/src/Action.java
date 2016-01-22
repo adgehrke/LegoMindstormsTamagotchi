db89624326c58c4a0985c16eeb3e213043a4d9bd
mbroelem <m.broelemann@gmx.de>
2016-01-22 16:24:54 +0100

    class Action and class Input

---
+++ LCD/src/Action.java
@@ -0,0 +1,69 @@
+
+public class Action {
+
+	
+	/*
+	 * Kopf: Kopf runter, Kopf leicht auf/ab
+	 * Sound: Ess-Sound
+	 * 
+	 * Endet, wenn Essen weg (Farbsensor)
+	 * 
+	 */
+	public void eating(){
+		
+	}
+	
+	/*
+	 * Keine Bewegung
+	 * Display: Augen zu
+	 * Sound: Schnarchen
+	 * 
+	 * Nach 1 min, schreit er, bis er wieder gerade ist.
+	 * Endet, wenn er wieder aufgestellt ist.
+	 * 
+	 */
+	public void sleeping(){
+		
+	}
+	
+	/*
+	 * Bewegung: Fährt gradeaus, bis schwarze-Randlinie, dreht sich min 90° max 270°
+	 * Kopf: stabil eher unten
+	 * Sound: singsang-summen
+	 * Display: Augen Auf. (Pupillenbewegung)
+	 * 
+	 * Endet nach max 1min, oder wenn Ball gefunden.
+	 * 
+	 */
+	public void playing(){
+		
+	}
+	
+	
+	/*
+	 * 
+	 * Sound: glücklich summen (aufsteigend)
+	 * Display: Augen öffnen sich langsam
+	 * 
+	 * Endet, sofort nach Spritze
+	 * 
+	 */
+	public void healing(){
+		
+	}
+	
+	/*
+	 * 
+	 * Sound: nach 2-Mal streicheln: Schreien, danach zufrieden summen
+	 * Bewegen: bei 2-Mal streicheln: kurzes rückfahren, danach stillstand
+	 * Kopf: bei 2-Mal unzufrieden kopf runter: danach: Kopf hoch
+	 * Display: Augen stabil.
+	 * 
+	 * Endet, wenn Button nach 3 Sekunden noch nicht wieder gedrückt wurde
+	 * 
+	 */
+	public void washing(){
+		
+	}
+	
+}
---
+++ LCD/src/Input.java
@@ -0,0 +1,54 @@
+
+public class Input {
+
+	/*
+	 * 
+	 * Bunte Farben unter Color-Sensor
+	 * call Action:eating
+	 */
+	public void giveFood(){
+		
+	}
+	
+	
+	/*
+	 * Button pressed (Spritze geben)
+	 * call Action:healing
+	 */
+	public void heal(){
+		
+	}
+	
+	
+	/*
+	 * 
+	 * Button-Pressed (Streicheln)
+	 * call Action:washing
+	 * 
+	 */
+	public void clean(){
+		
+	}
+	
+	
+	/*
+	 * 
+	 * Blauer Boden: spielzimmer
+	 * call Action:playing()
+	 * 
+	 */
+	public void play(){
+		
+	}
+	
+	
+	/*
+	 * 
+	 * Auf die Seite-Drehen: Gyro-Sensor
+	 * call Action:sleeping()
+	 * 
+	 */
+	public void bedtime(){
+		
+	}
+}
